390e9ddc7be2de71298169ebc079cae8f028b425
Ryan Troy Lattibeaudiere <r_latti@hotmail.com>
2019-10-29 16:00:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,9 +13,10 @@ end
 
 introduction_with_language('Dan','Ember.js')
 
-
-
-
+introduction_with_language_optional(name,language)
+  puts "Hi my name is #{name} and I am learning to in Ruby."
+end
+introduction_with_language_optional('Edwin',language)
 
 
 
